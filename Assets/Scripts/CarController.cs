9a1a0a62609d85f5ec806018ba6ceaf603ba1864
Louis <louisemard17@gmail.com>
2019-03-30 23:13:33 +0100

    zaer

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -6,8 +6,12 @@ public class CarController : MonoBehaviour
     public float maxspeed = 5;
     public float turnpower = 2;
     public float friction = 3;
+    public float accelMax = 3;
+    public float currentacel = 1f;
+    public float accelGap = 0.01f;
     private Rigidbody2D rigidbody2D;
     private Vector2 carSpeed;
+
 
     private void Start()
     {
@@ -16,8 +20,8 @@ public class CarController : MonoBehaviour
 
     private void FixedUpdate()
     {
-
-
+        GameObject.Find("WPTrail1").GetComponent<TrailRenderer>().emitting = false;
+        GameObject.Find("WPTrail2").GetComponent<TrailRenderer>().emitting = false;
         if (carSpeed.magnitude > maxspeed)
         {
             carSpeed = carSpeed.normalized;
@@ -26,10 +30,19 @@ public class CarController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Z))
         {
-            rigidbody2D.AddForce( transform.up * power);
+            rigidbody2D.AddForce(transform.up * power * currentacel);
             rigidbody2D.drag = friction;
-            Debug.Log("Z pressed");
+            // if (currentacel <= accelMax)
+            // {
+            //     currentacel += accelGap;
+            // }
         }
+        // else
+        // {
+        //     if(currentacel > 0){
+        //         currentacel -= accelGap;
+        //     }
+        // }
         if (Input.GetKey(KeyCode.S))
         {
             rigidbody2D.AddForce(-(transform.up) * (power / 2));
@@ -38,10 +51,17 @@ public class CarController : MonoBehaviour
         if (Input.GetKey(KeyCode.Q))
         {
             transform.Rotate(Vector3.forward * turnpower);
+            GameObject.Find("WPTrail1").GetComponent<TrailRenderer>().emitting = true;
+            GameObject.Find("WPTrail2").GetComponent<TrailRenderer>().emitting = true;
+            cam.GetComponent<TestScreenShake>().TriggerShake();
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(Vector3.forward * -turnpower);
+            GameObject.Find("WPTrail1").GetComponent<TrailRenderer>().emitting = true;
+            GameObject.Find("WPTrail2").GetComponent<TrailRenderer>().emitting = true;
+            cam.GetComponent<TestScreenShake>().TriggerShake();
+
         }
     }
 }
