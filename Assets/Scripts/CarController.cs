bc23e56b5f2342876fef166520cd1786faec1db5
Victor <victor.dumont64@gmail.com>
2019-05-20 14:36:18 +0200

    Voiture connecté

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,83 +1,93 @@
-﻿using UnityEngine;
+﻿using Photon.Pun;
+using UnityEngine;
 
-public class CarController : MonoBehaviour
-{
-    public float power = 3;
-    public float maxspeed = 5;
-    public float turnpower = 2;
-    public float friction = 3;
-    public float accelMax = 3;
-    public float currentacel = 1f;
-    public float accelGap = 0.01f;
-    private Rigidbody rigidbody;
-    private Vector2 carSpeed;
+public class CarController : MonoBehaviourPun {
+  public float power = 3;
+  public float maxspeed = 5;
+  public float turnpower = 2;
+  public float friction = 3;
+  public float accelMax = 3;
+  public float currentacel = 1f;
+  public float accelGap = 0.01f;
+  private Rigidbody rigidbody;
+  private Vector2 carSpeed;
+  [Tooltip ("The local player instance. Use this to know if the local player is represented in the Scene")]
+  public static GameObject LocalPlayerInstance;
 
+  private void Awake () {
+    // #Important
+    // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+    if (photonView.IsMine) {
+      CarController.LocalPlayerInstance = this.gameObject;
+    }
+    // #Critical
+    // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+    DontDestroyOnLoad (this.gameObject);
+  }
 
+  private void Start () {
+    rigidbody = GetComponent<Rigidbody> ();
+    CameraWork _cameraWork = this.gameObject.GetComponent<CameraWork> ();
 
-    private void Start()
-    {
-        rigidbody = GetComponent<Rigidbody>();
+    if (_cameraWork != null) {
+      if (photonView.IsMine) {
+        _cameraWork.OnStartFollowing ();
+      }
+    } else {
+      Debug.LogError ("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
+    }
+  }
+
+  private void FixedUpdate () {
+    if (photonView.IsMine == false && PhotonNetwork.IsConnected == true) {
+      return;
+    }
+    GameObject.Find ("WPTrail1").GetComponent<TrailRenderer> ().emitting = false;
+    GameObject.Find ("WPTrail2").GetComponent<TrailRenderer> ().emitting = false;
+    if (carSpeed.magnitude > maxspeed) {
+      carSpeed = carSpeed.normalized;
+      carSpeed *= maxspeed;
     }
 
-    private void FixedUpdate()
-    {
-        GameObject.Find("WPTrail1").GetComponent<TrailRenderer>().emitting = false;
-        GameObject.Find("WPTrail2").GetComponent<TrailRenderer>().emitting = false;
-        if (carSpeed.magnitude > maxspeed)
-        {
-            carSpeed = carSpeed.normalized;
-            carSpeed *= maxspeed;
-        }
-
-        if (Input.GetKey(KeyCode.Z))
-        {
-            Up();
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            Down();
-        }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            Left();
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            Right();
-        }
+    if (Input.GetKey (KeyCode.Z)) {
+      Up ();
     }
-
-    public void Up()
-    {
-        rigidbody.AddForce(transform.forward * power);
-        rigidbody.drag = friction;
+    if (Input.GetKey (KeyCode.S)) {
+      Down ();
     }
-
-    public void Down()
-    {
-        rigidbody.AddForce(-(transform.forward) * (power / 2));
-        rigidbody.drag = friction;
+    if (Input.GetKey (KeyCode.Q)) {
+      Left ();
     }
-
-    public void Left()
-    {
-        if (GetComponent<Rigidbody>().velocity.x != 0 || GetComponent<Rigidbody>().velocity.z != 0)
-        {
-
-            transform.Rotate(Vector3.up  * -turnpower);
-            GameObject.Find("WPTrail1").GetComponent<TrailRenderer>().emitting = true;
-            GameObject.Find("WPTrail2").GetComponent<TrailRenderer>().emitting = true;
-        }
+    if (Input.GetKey (KeyCode.D)) {
+      Right ();
     }
+  }
 
-    public void Right()
-    {
-        Debug.Log("Rotate called");
-        if (GetComponent<Rigidbody>().velocity.x != 0 || GetComponent<Rigidbody>().velocity.z != 0)
-        {
-            transform.Rotate(Vector3.up * turnpower);
-            GameObject.Find("WPTrail1").GetComponent<TrailRenderer>().emitting = true;
-            GameObject.Find("WPTrail2").GetComponent<TrailRenderer>().emitting = true;
-        }
+  public void Up () {
+    rigidbody.AddForce (transform.forward * power);
+    rigidbody.drag = friction;
+  }
+
+  public void Down () {
+    rigidbody.AddForce (-(transform.forward) * (power / 2));
+    rigidbody.drag = friction;
+  }
+
+  public void Left () {
+    if (GetComponent<Rigidbody> ().velocity.x != 0 || GetComponent<Rigidbody> ().velocity.z != 0) {
+
+      transform.Rotate (Vector3.up * -turnpower);
+      GameObject.Find ("WPTrail1").GetComponent<TrailRenderer> ().emitting = true;
+      GameObject.Find ("WPTrail2").GetComponent<TrailRenderer> ().emitting = true;
     }
+  }
+
+  public void Right () {
+    Debug.Log ("Rotate called");
+    if (GetComponent<Rigidbody> ().velocity.x != 0 || GetComponent<Rigidbody> ().velocity.z != 0) {
+      transform.Rotate (Vector3.up * turnpower);
+      GameObject.Find ("WPTrail1").GetComponent<TrailRenderer> ().emitting = true;
+      GameObject.Find ("WPTrail2").GetComponent<TrailRenderer> ().emitting = true;
+    }
+  }
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Com.MyCompany.MyGame {
+  public class GameManager : MonoBehaviourPunCallbacks {
+
+    [Tooltip ("The prefab to use for representing the player")]
+    public GameObject playerPrefab;
+
+    #region Photon Callbacks
+
+    /// <summary>
+    /// Called when the local player left the room. We need to load the launcher scene.
+    /// </summary>
+    public override void OnLeftRoom () {
+      SceneManager.LoadScene (0);
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    private void Start () {
+      if (playerPrefab == null) {
+        Debug.LogError ("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+      } else {
+        if (CarController.LocalPlayerInstance == null) {
+          Debug.LogFormat ("INSTANCIATION");
+          // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+          PhotonNetwork.Instantiate (this.playerPrefab.name, new Vector3 (0f, 0f, 0f), Quaternion.identity, 0);
+        } else {
+          Debug.LogFormat("DEJA INSTANCIE");
+        }
+      }
+    }
+
+    public void LeaveRoom () {
+      PhotonNetwork.LeaveRoom ();
+    }
+
+    void LoadArena () {
+      if (!PhotonNetwork.IsMasterClient) {
+        Debug.LogError ("PhotonNetwork : Trying to Load a level but we are not the master Client");
+      }
+      PhotonNetwork.LoadLevel ("ProtoVoiture");
+    }
+
+    #endregion
+
+  }
+}
