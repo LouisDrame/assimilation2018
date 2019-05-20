54bfdf19cc1f192b07fddc9a524f2fdc2a6f24f0
Louis <louisemard17@gmail.com>
2019-05-20 14:00:17 +0200

    #4 Test powerup

---
+++ Assets/Scripts/PropsPowerUps/TestPowerUp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPowerUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log("collision");
+        if(other.gameObject.GetComponent<CarController>() != null){
+            other.gameObject.GetComponent<CarController>().power = other.gameObject.GetComponent<CarController>().power * 10;
+            Application.Quit();
+            Destroy(gameObject);
+        }
+    }
+}
