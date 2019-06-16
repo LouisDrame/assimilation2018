12115957e1a4218c80a8cf49690d3b736a32a01e
Louis <louisemard17@gmail.com>
2019-06-16 19:01:54 +0200

    #9 Création sons + UI jeu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using Photon.Pun;
 using Photon.Realtime;
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    private Button buttonClicked;
+    public bool quit; /*!< Script utilisé pour la fin de jeu pour quitter ou revenir au menu, permet d'utiliser un seul scritp pour les deux actions */
+    // Start is called before the first frame update
+    void Start()
+    {
+        buttonClicked = GetComponent<Button>();
+        if(quit){
+            buttonClicked.onClick.AddListener(QuitGame);
+        }
+        else{
+            buttonClicked.onClick.AddListener(GoToMenu);
+        }
+    }
+
+    private void QuitGame(){
+        Debug.Log("Quit App");
+        Application.Quit();
+    }
+
+    private void GoToMenu(){
+        // SceneManager.LoadScene(0);
+        PhotonNetwork.LeaveRoom();
+    }
+
+    void OnLeftRoom(){
+        SceneManager.LoadScene(0);
+    }
+}
